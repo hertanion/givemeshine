86d8231e41ab79e10dde5dc048cd4f34d795bc6b
whyrinki <naer200469@gmail.com>
2021-09-19 10:41:05 +0300

    move emoji to separate file

---
+++ src/utils/emojis.ts
@@ -0,0 +1,7 @@
+
+export const shine_emoji = '✨';
+export const passed_emoji = '✔️';
+export const not_passed_emoji = '❌';
+export const list_emoji = '📃';
+export const module_emoji = '📦';
+export const command_emoji = '📝';
